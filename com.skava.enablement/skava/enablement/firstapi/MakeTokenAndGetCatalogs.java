faed7888810a491e1e5243d93fe42ee80141e95e
David W. Levine <david.levine@skava.com>
2019-06-17 03:40:59 -0400

    Initial push for core content of first-api

---
+++ com.skava.enablement/skava/enablement/firstapi/MakeTokenAndGetCatalogs.java
@@ -0,0 +1,163 @@
+/* 
+ * (C) Copyright Kalidus DBA Skava, 2018 all rights reserved
+ * 
+ * This code is provided on an as-is basis for the illustration of invoking the Skava API. It is not warranted to be fit for any other purpose. 
+ * This code is not optimized for production purposes. 
+ *  
+ * 
+ */
+package skava.enablement.firstapi;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+// Used to generate the JWT 
+import org.springframework.security.core.token.Sha512DigestUtils;
+import org.springframework.web.client.HttpClientErrorException;
+import com.google.gson.Gson;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+// Used to call the catalog service
+import com.skava.commerce.catalog.client.api.CatalogsApi;
+import com.skava.commerce.catalog.client.invoker.ApiClient;
+import com.skava.commerce.catalog.client.model.CatalogsAPIResponse;
+
+
+public class MakeTokenAndGetCatalogs {
+	/**
+	 * 
+	 * This sets up the API to call the proper instance, and populates a default
+	 * header with the API key required to access our services through the cloud
+	 * deployment Web Application Firewall
+	 * 
+	 * @param sfDetails
+	 * @return
+	 */
+
+	private static ApiClient setupApi(StorefrontDetails sfDetails) {
+		ApiClient apiClient = new ApiClient();
+		apiClient.setBasePath("https://" + sfDetails.getInstanceName() + "/catalogservices");
+		apiClient.setDebugging(true);
+		apiClient.addDefaultHeader("x-api-key", sfDetails.getApiKey());
+		return apiClient;
+	}
+   /**
+    *
+    * Use the Skava SDK to fetch the catalogs for a given store. Details are passed in via the 'StorefrontDescription" class
+    * 
+    * @param sfd
+    */
+	public static void getCatalogs(StorefrontDetails sfd) {
+		ApiClient apiClient = setupApi(sfd);
+		CatalogsApi api = new CatalogsApi(apiClient);
+		Long xCollectionId = sfd.getCollection();
+		String xAuthToken = sfd.getAuthToken();
+		String xVersion = null;
+		Integer page = 1;
+		Integer size = 50;
+		String sort = null;
+		String filters = null;
+		String locale = null;
+		CatalogsAPIResponse response = null;
+		try {
+			response = api.getCatalogs(xCollectionId, xAuthToken, xVersion, page, size, sort, filters, locale);
+		} catch (HttpClientErrorException e) {
+			System.out.println("Error " + e.getRawStatusCode() + " when calling getCatalogs");
+			System.out.println(e.getResponseBodyAsString());
+			return;
+		} catch (Exception e) {
+			System.out.println("Exception while calling getCatalogs " + e.getMessage());
+		}
+		System.out.println("Catalogs response: " + response.toString());
+	}
+
+	/**
+	 * 
+	 * Create a valid JWT to access the skava microservices
+	 * 
+	 * @param sharedSecret
+	 * @return
+	 */
+	
+	public static String generateJWT(String sharedSecret) {
+
+		String userid = "xxx";
+		Map<String, Object> claims = new HashMap<String, Object>();
+		claims.put("authorities",
+				"[{\"roles\":{\"ROLE_BUSINESS_ADMIN\":{\"business\":{\"50\":{}},\"type\":\"STANDARD\"}}}]");
+		claims.put("username", userid);
+		claims.put("created", Long.valueOf(System.currentTimeMillis()));
+		Gson gson = new Gson();
+		String claimsAsJson = gson.toJson(claims, Map.class);
+		System.out.println("Claims as json " + claimsAsJson);
+		String key = generateKeyWithClaims(sharedSecret, claims, userid);
+		System.out.println("admin key with claims " + key);
+		return key;
+	}
+
+	/**
+	 * This code does the hashed signing and calls the jwt.io library top create a valid token 
+	 * @param sharedSecret
+	 * @param claims
+	 * @param userid
+	 * @return
+	 */
+	
+	private static String generateKeyWithClaims(String sharedSecret, Map<String, Object> claims, String userid) {
+		return Jwts.builder().setSubject(userid).setClaims(claims).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 86400000)).setClaims(claims)
+				.signWith(SignatureAlgorithm.HS512, Sha512DigestUtils.shaHex(sharedSecret)).compact();
+
+	}
+
+	/**
+	 * This main solicits the inputs, and calls the methods to:
+	 *  	* generate the JWT
+	 *      * get the catlogs for this business 
+	 * 
+	 * 
+	 * @param args
+	 * @throws URISyntaxException
+	 * @throws IOException
+	 */
+	
+	public static void main(String[] args) throws URISyntaxException, IOException {
+		StorefrontDetails sfd = new StorefrontDetails();
+		Scanner scan = new Scanner(System.in);
+		String instance = "stratus.skavacommerce.com";
+		System.out.println("Enter Shared Secret : ");
+		String sharedSecret = scan.nextLine();
+		String authToken = generateJWT(sharedSecret);
+		sfd.setAuthToken(authToken);
+		System.out.println("Enter api key:");
+		String apiKey = scan.nextLine();
+		
+		System.out.println("enter business:[50]");
+		String businessId = scan.nextLine();
+		if (businessId.equals("")) {
+			businessId = "50";
+		}
+		long bid = new Long(businessId);
+		sfd.setBusiness(bid);
+		System.out.println("Enter collection[268]:");
+		String collection = scan.nextLine();
+		if (collection.equals("")) {
+			collection = "268";
+		}
+		long coll = new Long(collection);
+		sfd.setBusiness(bid);
+		sfd.setCollection(coll);
+		sfd.setInstanceName(instance);
+		sfd.setApiKey(apiKey);
+		scan.close();
+		getCatalogs(sfd);
+
+	}
+
+}
---
+++ com.skava.enablement/skava/enablement/firstapi/StorefrontDetails.java
@@ -0,0 +1,121 @@
+package skava.enablement.firstapi;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.google.gson.Gson;
+
+public class StorefrontDetails {
+	
+	private String instanceName; 
+	private long business;
+	private long store;
+	private long collection;
+	private String authToken;
+	private String sessionId;
+	private String sharedSecret;
+	private String catalog;
+	private String project;
+	private String apiKey;
+	
+	public String getApiKey() {
+		return apiKey;
+	}
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+	public String getCatalog() {
+		return catalog;
+	}
+	public void setCatalog(String catalog) {
+		this.catalog = catalog;
+	}
+	public String getProject() {
+		return project;
+	}
+	public void setProject(String project) {
+		this.project = project;
+	}
+	public String getSharedSecret() {
+		return sharedSecret;
+	}
+	public void setSharedSecret(String sharedSecret) {
+		this.sharedSecret = sharedSecret;
+	}
+	public String getAuthToken() {
+		return authToken;
+	}
+	public void setAuthToken(String authToken) {
+		this.authToken = authToken;
+	}
+	public String getSessionId() {
+		return sessionId;
+	}
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+	public String getInstanceName() {
+		return instanceName;
+	}
+	public void setInstanceName(String instanceName) {
+		this.instanceName = instanceName;
+	}
+	public long getBusiness() {
+		return business;
+	}
+	public void setBusiness(long business) {
+		this.business = business;
+	}
+	public long getStore() {
+		return store;
+	}
+	public void setStore(long store) {
+		this.store = store;
+	}
+	public long getCollection() {
+		return collection;
+	}
+	public void setCollection(long collection) {
+		this.collection = collection;
+	}
+	
+	public String toString()
+	{
+	Gson gson = new Gson();
+	String jsonString = gson.toJson(this);
+	return jsonString;	
+	}
+
+	private static String readFromInputStream(InputStream inputStream)
+			  throws IOException {
+			    StringBuilder resultStringBuilder = new StringBuilder();
+			    try (BufferedReader br
+			      = new BufferedReader(new InputStreamReader(inputStream))) {
+			        String line;
+			        while ((line = br.readLine()) != null) {
+			            resultStringBuilder.append(line).append("\n");
+			        }
+			    }
+			  return resultStringBuilder.toString();
+			}
+	
+	public static StorefrontDetails buildFromFile(String filename)
+	{
+	Class clazz = StorefrontDetails.class;
+    InputStream inputStream = clazz.getResourceAsStream(filename);
+    String jsonFromFile;
+	try {
+		jsonFromFile = readFromInputStream(inputStream);
+	} catch (IOException e) {
+		System.out.println("Error reading from file "+filename);
+		return null;
+	}
+    Gson gson = new Gson();
+    StorefrontDetails constructedSfd = gson.fromJson(jsonFromFile, StorefrontDetails.class);
+	return constructedSfd;	
+	}
+	
+	
+}
